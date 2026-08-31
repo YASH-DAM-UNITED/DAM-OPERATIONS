2fb38e013ba2ad31e7e673386f031f456c183fd9
YASH <yash@damunited.sa>
2026-08-31 12:10:29 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,25 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+  ],
+
   build: {
-    outDir: "dist",
+    rollupOptions: {
+      input: {
+        main: resolve(
+          __dirname,
+          "index.html"
+        ),
+
+        mooma: resolve(
+          __dirname,
+          "mooma/index.html"
+        ),
+      },
+    },
   },
 });
